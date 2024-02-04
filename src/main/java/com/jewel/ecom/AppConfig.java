bd540d9b9cdefa583ffb3a3500a7b786ba36afb5
jewel73 <jewelranar420@gmail.com>
2024-02-04 18:22:03 +0600

    Etag shallowetagheader filter bean

---
+++ src/main/java/com/jewel/ecom/AppConfig.java
@@ -0,0 +1,14 @@
+package com.jewel.ecom;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ShallowEtagHeaderFilter shallowEtagHeaderFilter(){
+        return new ShallowEtagHeaderFilter();
+    }
+}
